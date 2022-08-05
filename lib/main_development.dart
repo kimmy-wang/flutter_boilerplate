d5a32152e148917f9b3fc9f685c4301ec46bfb7e
see <upcwangying@126.com>
2022-08-05 14:32:03 +0800

    refactor: 项目框架重构

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,13 +1,13 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
-import 'package:flutter_boilerplate/app/app.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_boilerplate/bootstrap.dart';
+import 'package:local_storage_todos_api/local_storage_todos_api.dart';
 
-void main() {
-  bootstrap(() => const App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final todosApi = LocalStorageTodosApi(
+    plugin: await SharedPreferences.getInstance(),
+  );
+
+  bootstrap(todosApi: todosApi);
 }
