03ba00a4f5e24a1c02bbe2a371d458123f6ce1d6
see <upcwangying@126.com>
2022-09-02 15:42:37 +0800

    refactor: mine

--- lib/modules/mine/views/mine_page.dart
+++ lib/modules/mine/views/mine_page.dart
@@ -1,8 +1,10 @@
-import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_boilerplate/common/utils/navigator_util.dart';
 import 'package:flutter_boilerplate/l10n/l10n.dart';
 import 'package:flutter_boilerplate/modules/app/cubit/app_cubit.dart';
+import 'package:flutter_boilerplate/modules/mine/mine.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class MinePage extends StatelessWidget {
   const MinePage({super.key});
@@ -19,88 +21,46 @@ class MineView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: [
-            const Padding(padding: EdgeInsets.only(top: 20)),
-            Text(
-              context.l10n.title,
-              style: const TextStyle(fontSize: 20),
-            ),
-            const Padding(padding: EdgeInsets.only(top: 20)),
-            IconButton(
-              onPressed: () async {
-                await showTranslateActionSheet(context);
-              },
-              icon: const Icon(Icons.translate),
-            ),
-            const Padding(padding: EdgeInsets.only(top: 20)),
-            BlocBuilder<AppCubit, AppState>(
-              builder: (BuildContext context, AppState state) => IconButton(
-                onPressed: () async {
-                  await showThemeModeActionSheet(context);
-                },
-                icon: Icon(
-                  state.themeMode == ThemeMode.system
-                      ? Icons.tonality
-                      : state.themeMode == ThemeMode.light
-                          ? Icons.light_mode
-                          : Icons.dark_mode,
+      body: BlocBuilder<AppCubit, AppState>(
+        builder: (BuildContext context, AppState state) => SettingsList(
+          sections: [
+            SettingsSection(
+              title: const Text('APPEARANCE'),
+              tiles: [
+                SettingsTile.navigation(
+                  onPressed: (_) {
+                    NavigatorUtil.push(context, const ThemeModePage());
+                  },
+                  title: const Text('Dark Appearance'),
+                  value: Text(
+                    state.themeMode == ThemeMode.system
+                        ? '跟随系统'
+                        : state.themeMode == ThemeMode.dark
+                            ? '黑暗模式'
+                            : '白天模式',
+                  ),
                 ),
-              ),
+              ],
+            ),
+            SettingsSection(
+              title: const Text('LANGUAGES'),
+              tiles: [
+                SettingsTile.navigation(
+                  onPressed: (_) {
+                    NavigatorUtil.push(context, const LanguagesPage());
+                  },
+                  title: const Text('LANGUAGE'),
+                  value: Text(
+                    state.locale.languageCode == 'zh'
+                        ? context.l10n.languagesZh
+                        : context.l10n.languagesEn,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
     );
-  }
-
-  Future<void> showTranslateActionSheet(BuildContext context) async {
-    final result = await showModalActionSheet<String>(
-      context: context,
-      title: context.l10n.languages,
-      actions: [
-        SheetAction(
-          icon: Icons.info,
-          label: context.l10n.languagesZh,
-          key: 'zh',
-        ),
-        SheetAction(
-          icon: Icons.warning,
-          label: context.l10n.languagesEn,
-          key: 'en',
-        ),
-      ],
-    );
-    if (result != null) {
-      BlocProvider.of<AppCubit>(context).setLocale(Locale(result));
-    }
-  }
-
-  Future<void> showThemeModeActionSheet(BuildContext context) async {
-    final result = await showModalActionSheet<ThemeMode>(
-      context: context,
-      actions: const [
-        SheetAction(
-          icon: Icons.tonality,
-          label: '跟随系统',
-          key: ThemeMode.system,
-        ),
-        SheetAction(
-          icon: Icons.dark_mode,
-          label: '黑暗模式',
-          key: ThemeMode.dark,
-        ),
-        SheetAction(
-          icon: Icons.light_mode,
-          label: '白天模式',
-          key: ThemeMode.light,
-        ),
-      ],
-    );
-    if (result != null) {
-      BlocProvider.of<AppCubit>(context).setThemeMode(result);
-    }
   }
 }
